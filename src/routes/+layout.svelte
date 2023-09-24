b0ecee94d07a3d2eb161b348140c4913a59b949d
Andrea Bianchi <bianan96@gmail.com>
2023-09-24 17:35:05 +0200

    Readd footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,8 @@
 	<main>
 		<slot />
 	</main>
+
+	<footer>This is just a silly app to break the ice with Svelte, nothing too fancy.</footer>
 </div>
 
 <style>
@@ -27,5 +29,19 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
 	}
 </style>
